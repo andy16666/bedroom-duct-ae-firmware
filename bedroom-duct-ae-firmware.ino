3a1dece505d2328f5ee0b626701da46d4ab73732
Andrew Somerville <andrew@andrewonline.ca>
2025-06-03 23:39:57 -0300

    Performance and refactoring. Added humidity improvements.

--- bedroom-duct-ae-firmware.ino
+++ bedroom-duct-ae-firmware.ino
@@ -20,37 +20,30 @@
     Author: Andrew Somerville <andy16666@gmail.com> 
     GitHub: andy16666
  */
-#include <Arduino_JSON.h>
-#include <LEAmDNS.h>
+
+#include <stdint.h>
+#include <float.h>
+#include <math.h>
+#include <string.h>
+
+#include <CPU.h>
 #include <WebServer.h>
 #include <HTTPClient.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
-#include <stdint.h>
-#include <float.h>
-#include <math.h>
-#include "config.h"
-#include <CPU.h>
-#include <string.h>
+
+#include <aos.h>
+#include <config.h>
 #include <GPIOOutputs.h>
 #include <Thermostats.h>
-#include <TemperatureSensors.h>
-#include <GPIOOutputs.h>
-#include <util.h>
 #include <SimplicityAC.h> 
-extern "C" {
-#include <threadkernel.h>
-};
 
-using namespace AOS; 
+using namespace AOS;
+
 using AOS::Thermostat; 
 using AOS::Thermostats;
-
-#define TEMP_SENSOR_PIN 2
-
-#define PING_INTERVAL_MS 5000
-#define SENSOR_READ_INTERVAL_MS 6000
-#define MAX_CONSECUTIVE_FAILED_PINGS 5
+using AOS::TemperatureSensors;
+using AOS::Ping; 
 
 #define HRV_LOW_EXHAUST  6
 #define HRV_LOW_INTAKE   7
@@ -62,66 +55,24 @@ using AOS::Thermostats;
 #define ENTRYWAY_BYPASS   12
 #define LR_DUCT_BOOST     13
 
-#define CORE_0_ACT   18
-#define CORE_1_ACT     19
-
 #define TRANSITION_TIME_MS 2000
 
 #define HRV_INTAKE_INLET_TEMP_ADDR  139
 #define HRV_INTAKE_OUTLET_TEMP_ADDR 131
 #define HRV_EXHAUST_INLET_TEMP_ADDR 191
 #define HRV_EXHAUST_OUTLET_TEMP_ADDR 205
-//#define INTAKE_TEMP_ADDR 83
-//#define LR_OUTLET_TEMP_ADDR 226
-//#define FEW_OUTLET_TEMP_ADDR 181
 #define INTAKE_TEMP_ADDR 12
 #define LR_OUTLET_TEMP_ADDR 235
 #define FEW_OUTLET_TEMP_ADDR 176
 
-CPU cpu; 
-TemperatureSensors TEMPERATURES(TEMP_SENSOR_PIN);
 GPIOOutputs BLOWERS("Blowers");
 WebServer server(80);
-AOS::Thermostats thermostats;
 SimplicityAC acData(AC_HOSTNAME); 
+AOS::Thermostats thermostats;
 
 const char* LIVING_ROOM    = "lr"; 
 const char* MASTER_BEDROOM = "mb"; 
 const char* FRONT_ENTRYWAY = "few"; 
-const char* STATUS_JSON_FORMAT = 
-  "{\n"\
-  "  \"acData\":{\n" \
-  "    \"evapTempC\":\"%f\",\n" \
-  "    \"outletTempC\":\"%f\",\n" \
-  "    \"lastUpdated\":\"%s\",\n" \
-  "    \"command\":\"%c\",\n" \
-  "    \"state\":\"%c\",\n" \
-  "    \"fanState\":\"%c\",\n" \
-  "    \"compressorState\":\"%d\"\n" \
-  "  },\n" \
-  "  \"hrvIntakeInletTempC\":\"%s\",\n" \
-  "  \"hrvIntakeOutletTempC\":\"%s\",\n" \
-  "  \"hrvExhaustInletTempC\":\"%s\",\n" \
-  "  \"hrvExhaustOutletTempC\":\"%s\",\n" \
-  "  \"intakeTempC\":\"%s\",\n" \
-  "  \"lrOutletTempC\":\"%s\",\n" \
-  "  \"fewOutletTempC\":\"%s\",\n" \
-  "  \"hrvCommand\":\"%c\",\n" \
-  "  \"lrDuctCommand\":\"%c\",\n" \
-  "  \"fewDuctCommand\":\"%c\",\n" \
-  "  \"cpuTempC\":\"%f\",\n" \
-  "  \"tempErrors\":%d,\n" \
-  "  \"thermostats\":%s,\n" \
-  "  \"lastPing\":\"%fms\",\n" \
-  "  \"consecutiveFailedPings\":%d,\n" \
-  "  \"freeMem\":%d,\n" \
-  "  \"powered\":\"%s\",\n" \
-  "  \"booted\":\"%s\",\n" \
-  "  \"connected\":\"%s\",\n" \
-  "  \"numRebootsPingFailed\":%d,\n" \
-  "  \"numRebootsDisconnected\":%d,\n" \
-  "  \"wiFiStatus\":%d\n" \
-  "}\n";
 
 typedef enum {
   CMD_VENTILATE_HIGH = 'H',
@@ -142,68 +93,39 @@ typedef enum {
   CMD_ENTRYWAY_DUCT_OFF  = 'O'
 } few_duct_cmd_t;
 
-static volatile long            initialize             __attribute__((section(".uninitialized_data")));
-static volatile long            timeBaseMs             __attribute__((section(".uninitialized_data")));
-static volatile long            powerUpTime            __attribute__((section(".uninitialized_data")));
-static volatile long            tempErrors             __attribute__((section(".uninitialized_data")));
-static volatile long            numRebootsPingFailed   __attribute__((section(".uninitialized_data")));
-static volatile long            numRebootsDisconnected __attribute__((section(".uninitialized_data")));
-
 static volatile ventilate_cmd_t hrvCommand             __attribute__((section(".uninitialized_data")));
 static volatile lr_duct_cmd_t   lrDuctCommand          __attribute__((section(".uninitialized_data")));
 static volatile few_duct_cmd_t  fewDuctCommand         __attribute__((section(".uninitialized_data"))); 
 static volatile ac_cmd_t        acCommand              __attribute__((section(".uninitialized_data"))); 
+static volatile double          outletHumidity         __attribute__((section(".uninitialized_data")));
+static volatile double          ductIntakeHumidity     __attribute__((section(".uninitialized_data")));
+static volatile double          roomHumidity           __attribute__((section(".uninitialized_data")));
 
-volatile unsigned long startupTime = millis();
-volatile unsigned long connectTime = millis();
+volatile long coolingOffMs = millis(); 
 
-volatile unsigned long lastPingMicros         = 0; 
-volatile unsigned int  consecutiveFailedPings = 0; 
-
-threadkernel_t* CORE_0_KERNEL; 
-threadkernel_t* CORE_1_KERNEL;
-
-volatile char *httpJsonOutput; 
-
-
-
-void setup() 
+const char* generateHostname()
 {
-  CORE_0_KERNEL = create_threadkernel(&millis); 
+  return "hrv1"; 
+}
 
-  if (initialize)
-  {
-    timeBaseMs = 0; 
-    powerUpTime = millis(); 
-    tempErrors = 0; 
-    hrvCommand = CMD_VENTILATE_OFF; 
-    lrDuctCommand = CMD_LR_DUCT_OFF; 
-    fewDuctCommand = CMD_ENTRYWAY_DUCT_OFF; 
-    numRebootsPingFailed = 0; 
-    numRebootsDisconnected = 0; 
-    initialize = 0; 
-    acCommand = CMD_AC_OFF; 
-  }
+void aosInitialize()
+{
+  hrvCommand = CMD_VENTILATE_OFF; 
+  lrDuctCommand = CMD_LR_DUCT_OFF; 
+  fewDuctCommand = CMD_ENTRYWAY_DUCT_OFF; 
+  acCommand = CMD_AC_OFF; 
+  outletHumidity = 0; 
+  ductIntakeHumidity = 0;
+  roomHumidity = 0; 
+}
 
-  pinMode(CORE_0_ACT, OUTPUT); 
-
-  httpJsonOutput = 0; 
-
+void aosSetup()
+{
   thermostats.add(LIVING_ROOM);  
   thermostats.add(MASTER_BEDROOM); 
   thermostats.add(FRONT_ENTRYWAY); 
 
-  cpu.begin(); 
-
-  Serial.begin();
-  Serial.setDebugOutput(false);
-  
-  wifi_connect();
   server.begin();
-
-  if (MDNS.begin(HOSTNAME)) {
-    Serial.println("MDNS responder started");
-  }
 
   server.on("/", []() {
     bool success = true; 
@@ -212,6 +134,7 @@ void setup()
     {
       String argName = server.argName(i);
       String arg = server.arg(i);
+      bool success = false; 
 
       if (argName.equals("hrv") && arg.length() == 1) 
       {
@@ -295,8 +218,8 @@ void setup()
 
     if (success)
     {
-      char * buffer = (char *)httpJsonOutput; 
-      server.send(200, "text/json", buffer);
+      
+      server.send(200, "text/json", httpResponseString);
     }
     else 
     {
@@ -308,34 +231,20 @@ void setup()
   server.begin();
   Serial.println("HTTP server started");
   
-  CORE_0_KERNEL->addImmediate(CORE_0_KERNEL, task_mdnsUpdate); 
   CORE_0_KERNEL->addImmediate(CORE_0_KERNEL, task_handleClient); 
 
-  CORE_0_KERNEL->add(CORE_0_KERNEL, task_core0ActOn, 10); 
-  CORE_0_KERNEL->add(CORE_0_KERNEL, task_testPing, PING_INTERVAL_MS); 
-  CORE_0_KERNEL->add(CORE_0_KERNEL, task_testWiFiConnection, 5000); 
   CORE_0_KERNEL->add(CORE_0_KERNEL, task_processThermostatData, 15000); 
-  CORE_0_KERNEL->add(CORE_0_KERNEL, task_core0ActOff, 10); 
 }
 
-void loop() 
+void aosSetup1()
 {
-  CORE_0_KERNEL->run(CORE_0_KERNEL); 
-}
-
-void setup1()
-{
-  CORE_1_KERNEL = create_threadkernel(&millis); 
-  pinMode(CORE_1_ACT, OUTPUT); 
-
-  TEMPERATURES.add("HRV Intake Inlet", HRV_INTAKE_INLET_TEMP_ADDR); 
-  TEMPERATURES.add("HRV Intake Outlet", HRV_INTAKE_OUTLET_TEMP_ADDR); 
-  TEMPERATURES.add("HRV Exhaust Inlet", HRV_EXHAUST_INLET_TEMP_ADDR); 
-  TEMPERATURES.add("HRV Exhaust Outlet", HRV_EXHAUST_OUTLET_TEMP_ADDR); 
-  TEMPERATURES.add("Intake", INTAKE_TEMP_ADDR); 
-  TEMPERATURES.add("Living Room Outlet", LR_OUTLET_TEMP_ADDR); 
-  TEMPERATURES.add("Front Entryway Outlet", FEW_OUTLET_TEMP_ADDR);
-  TEMPERATURES.discoverSensors();
+  TEMPERATURES.add("HRV Intake Inlet", "hrvIntakeInletTempC", HRV_INTAKE_INLET_TEMP_ADDR); 
+  TEMPERATURES.add("HRV Intake Outlet", "hrvIntakeOutletTempC", HRV_INTAKE_OUTLET_TEMP_ADDR); 
+  TEMPERATURES.add("HRV Exhaust Inlet", "hrvExhaustInletTempC", HRV_EXHAUST_INLET_TEMP_ADDR); 
+  TEMPERATURES.add("HRV Exhaust Outlet", "hrvExhaustOutletTempC", HRV_EXHAUST_OUTLET_TEMP_ADDR); 
+  TEMPERATURES.add("Intake", "intakeTempC", INTAKE_TEMP_ADDR); 
+  TEMPERATURES.add("Living Room Outlet", "lrOutletTempC", LR_OUTLET_TEMP_ADDR); 
+  TEMPERATURES.add("Front Entryway Outlet", "fewOutletTempC", FEW_OUTLET_TEMP_ADDR);
 
   BLOWERS.add("HRV Low Exhaust", HRV_LOW_EXHAUST, false);
   BLOWERS.add("HRV Low Intake", HRV_LOW_INTAKE, false);
@@ -348,75 +257,23 @@ void setup1()
   BLOWERS.setAll();
 
   CORE_1_KERNEL->addImmediate(CORE_1_KERNEL, task_processCommands);  
-  CORE_1_KERNEL->add(CORE_1_KERNEL, task_core1ActOn, 10); 
-  CORE_1_KERNEL->add(CORE_1_KERNEL, task_readTemperatures, 10000); 
   CORE_1_KERNEL->add(CORE_1_KERNEL, task_updateNextBlower, TRANSITION_TIME_MS); 
-  CORE_1_KERNEL->add(CORE_1_KERNEL, task_bufferJsonOutput, 2000); 
-  CORE_1_KERNEL->add(CORE_1_KERNEL, task_core1ActOff, 10);
 }
 
-void loop1()
+void populateHttpResponse(JSONVar& document) 
 {
-  CORE_1_KERNEL->run(CORE_1_KERNEL); 
-}
+  document["intakeHumidity"] = ductIntakeHumidity; 
 
-void task_core0ActOn()
-{
-  digitalWrite(CORE_0_ACT, 1);
-}
+  document["hrvCommand"] = String((char)hrvCommand).c_str();
+  document["lrDuctCommand"] = String((char)lrDuctCommand).c_str();
+  document["fewDuctCommand"] = String((char)fewDuctCommand).c_str();
+  document["acCommand"] = String((char)acCommand).c_str();
 
-void task_core1ActOn()
-{
-  digitalWrite(CORE_1_ACT, 1);
-}
+  acData.addTo(AC_HOSTNAME, document); 
+  document[AC_HOSTNAME]["outletHumidity"] = outletHumidity; 
+  document[AC_HOSTNAME]["roomHumidity"]   = roomHumidity; 
 
-void task_core0ActOff()
-{
-  digitalWrite(CORE_0_ACT, 0);
-}
-
-void task_core1ActOff()
-{
-  digitalWrite(CORE_1_ACT, 0);
-}
-
-void task_bufferJsonOutput()
-{
-  char *oldJsonBuffer = (char *)httpJsonOutput; 
-  char *jsonBuffer = (char *)malloc(sizeof(char) * 2 * 1024); 
-  unsigned long timeMs = millis(); 
-  sprintf(jsonBuffer, STATUS_JSON_FORMAT,
-          acData.evapTempC,
-          acData.outletTempC, 
-          msToHumanReadableTime(timeMs - acData.updateTimeMs).c_str(),
-          acData.command, 
-          acData.state, 
-          acData.fanState, 
-          acData.compressorState,
-          TEMPERATURES.formatTempC(HRV_INTAKE_INLET_TEMP_ADDR).c_str(),
-          TEMPERATURES.formatTempC(HRV_INTAKE_OUTLET_TEMP_ADDR).c_str(),
-          TEMPERATURES.formatTempC(HRV_EXHAUST_INLET_TEMP_ADDR).c_str(),
-          TEMPERATURES.formatTempC(HRV_EXHAUST_OUTLET_TEMP_ADDR).c_str(),
-          TEMPERATURES.formatTempC(INTAKE_TEMP_ADDR).c_str(),
-          TEMPERATURES.formatTempC(LR_OUTLET_TEMP_ADDR).c_str(),
-          TEMPERATURES.formatTempC(FEW_OUTLET_TEMP_ADDR).c_str(),
-          hrvCommand,
-          lrDuctCommand,
-          fewDuctCommand,
-          cpu.getTemperature(),
-          tempErrors,
-          thermostats.toString().c_str(), 
-          lastPingMicros/1000.0,
-          consecutiveFailedPings,
-          getFreeHeap(),
-          msToHumanReadableTime((timeBaseMs + timeMs) - powerUpTime).c_str(), 
-          msToHumanReadableTime(timeMs - startupTime).c_str(),
-          msToHumanReadableTime(timeMs - connectTime).c_str(),
-          numRebootsPingFailed,
-          numRebootsDisconnected, 
-          3);
-  httpJsonOutput = jsonBuffer; 
-  free(oldJsonBuffer); 
+  thermostats.addTo("thermostats", document); 
 }
 
 void task_processThermostatData()
@@ -494,38 +351,66 @@ void task_processThermostatData()
 
   if (coolOff)
   {
-    if (acData.isOutletCold())
+    if (acData.isCooling())
     {
-      acCommand = acData.isCooling() ? CMD_AC_OFF : CMD_AC_FAN; 
+      coolingOffMs = millis(); 
     }
-    else if (acData.isEvapCold())
+
+    boolean coolingOffForMs = millis() - coolingOffMs; 
+
+    bool cmdACFanOn = acData.isOutletCold() || acData.isCooling(); 
+    bool cmdDuctFanOn = !acData.isOutletCold() && !acData.isCooling(); 
+    if (!acData.isCooling() && acData.isFanRunning() && TEMPERATURES.isTempValid(INTAKE_TEMP_ADDR))
     {
-      acCommand = acData.isCooling() ? CMD_AC_OFF : CMD_AC_FAN; 
+      double ductIntakeTempC = TEMPERATURES.getTempC(INTAKE_TEMP_ADDR);
+      if (acData.isEvapTempValid())
+      {
+        double evapTempC = acData.getEvapTempC(); 
+        ductIntakeHumidity = calculate_relative_humidity(ductIntakeTempC, evapTempC); 
+        cmdDuctFanOn = cmdDuctFanOn || ductIntakeHumidity > 75.0;  
+        if (acData.isOutletTempValid())
+        {
+          double outletTempC = acData.getOutletTempC(); 
+          outletHumidity = calculate_relative_humidity(outletTempC, evapTempC);
+          
+          cmdACFanOn   = cmdACFanOn || outletTempC < evapTempC || outletHumidity > 75.0; 
+        }
+
+        if (thermostats.contains(MASTER_BEDROOM) && thermostats.get(MASTER_BEDROOM).isCurrent())
+        {
+          double roomTemperatureC = thermostats.get(MASTER_BEDROOM).getCurrentTemperatureC(); 
+          roomHumidity = calculate_relative_humidity(roomTemperatureC, evapTempC); 
+        }
+      } 
+    }
+
+    cmdACFanOn = cmdACFanOn 
+        || coolingOffForMs < 30000 
+        || ductIntakeHumidity <= 0 
+        || outletHumidity <= 0 
+        || roomHumidity <= 0;
+    cmdDuctFanOn = cmdDuctFanOn 
+        || cmdACFanOn
+        || (TEMPERATURES.getTempC(LR_OUTLET_TEMP_ADDR) < 15 && TEMPERATURES.getTempC(INTAKE_TEMP_ADDR) > 15)
+        || TEMPERATURES.getTempC(INTAKE_TEMP_ADDR) < 15;
+
+    if (cmdACFanOn)
+    {
+      acCommand = acData.isCooling() ? CMD_AC_OFF : CMD_AC_FAN_LOW; 
     }
     else
     {
       acCommand = CMD_AC_KILL; 
     }
 
-    if (!acData.isOutletCold() && !acData.isCooling())
+    if (cmdDuctFanOn)
     {
-      if (TEMPERATURES.getTempC(INTAKE_TEMP_ADDR) < 15)
-      {
-        lrDuctCommand = CMD_LR_DUCT_LOW; 
-      }
-      else if (TEMPERATURES.getTempC(LR_OUTLET_TEMP_ADDR) < 15 && TEMPERATURES.getTempC(INTAKE_TEMP_ADDR) > 15)
-      {
-        lrDuctCommand = CMD_LR_DUCT_LOW; 
-      }
-      else 
-      {
-        lrDuctCommand = CMD_LR_DUCT_OFF;
-      }
+      lrDuctCommand = CMD_LR_DUCT_LOW; 
+    }
 
-      if (TEMPERATURES.getTempC(FEW_OUTLET_TEMP_ADDR) < 15 && TEMPERATURES.getTempC(INTAKE_TEMP_ADDR) > 15)
-      {
-        fewDuctCommand = CMD_ENTRYWAY_DUCT_HIGH; 
-      }
+    if (TEMPERATURES.getTempC(FEW_OUTLET_TEMP_ADDR) < 15 && TEMPERATURES.getTempC(INTAKE_TEMP_ADDR) > 15)
+    {
+      fewDuctCommand = CMD_ENTRYWAY_DUCT_HIGH; 
     }
   }
 
@@ -540,65 +425,14 @@ void task_handleClient()
   server.handleClient(); 
 }
 
-void task_mdnsUpdate()
-{
-  MDNS.update(); 
-}
-
-void task_testWiFiConnection()
-{
-  if (!is_wifi_connected()) 
-  {
-    Serial.println("WiFi disconnected, rebooting.");
-    numRebootsDisconnected++; 
-    reboot();  
-  }
-}
-
-void task_testPing()
-{
-  unsigned long timeMicros = micros(); 
-  int pingStatus = WiFi.ping(WiFi.gatewayIP(), 255); 
-  lastPingMicros = micros() - timeMicros; 
-  if (pingStatus < 0) 
-  {
-    if (++consecutiveFailedPings >= MAX_CONSECUTIVE_FAILED_PINGS)
-    {
-      Serial.println("Gateway unresponsive, rebooting.");
-      numRebootsPingFailed++; 
-      reboot(); 
-    }
-  }
-  else 
-  {
-    consecutiveFailedPings = 0; 
-  }
-}
-
 void task_processCommands()
 {
   processCommands(hrvCommand, lrDuctCommand, fewDuctCommand);
 }
 
-void task_readTemperatures()
-{
-  TEMPERATURES.readSensors();
-}
-
-void task_printTemperatures()
-{
-  TEMPERATURES.printSensors(); 
-}
-
 void task_updateNextBlower()
 {
   BLOWERS.setNext();
-}
-
-void reboot()
-{
-  timeBaseMs += millis(); 
-  rp2040.reboot(); 
 }
 
 void processCommands(ventilate_cmd_t hrvCommand, lr_duct_cmd_t lrDuctCommand, few_duct_cmd_t fewDuctCommand)
@@ -643,49 +477,6 @@ void processCommands(ventilate_cmd_t hrvCommand, lr_duct_cmd_t lrDuctCommand, fe
   BLOWERS.setCommand(ENTRYWAY_BYPASS, 
       hrvCommand == CMD_VENTILATE_HIGH 
       || fewDuctCommand == CMD_ENTRYWAY_DUCT_HIGH); 
-}
-
-// WIFI
-
-bool is_wifi_connected() 
-{
-  return WiFi.status() == WL_CONNECTED;
-}
-
-void wifi_connect() 
-{
-  WiFi.noLowPowerMode();
-  Serial.print("Connecting to ");
-  Serial.println(WIFI_SSID);
-
-  WiFi.mode(WIFI_STA);
-  WiFi.noLowPowerMode();
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-
-  int attempts = 20;
-  while (!is_wifi_connected() && (attempts--)) 
-  {
-    delay(1000);
-  }
-
-  if (!is_wifi_connected()) {
-    Serial.print("Connect failed! Rebooting.");
-    numRebootsDisconnected++; 
-    reboot(); 
-  }
-
-  Serial.println("Connected");
-  Serial.print("Connected to ");
-  Serial.println(WiFi.SSID());
-
-  Serial.print("IP: ");
-  Serial.println(WiFi.localIP());
-
-  Serial.print("Hostname: ");
-  Serial.print(HOSTNAME);
-  Serial.println(".local");
-
-  connectTime = millis();
 }
 
 void handleNotFound() {
